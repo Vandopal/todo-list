3ccda33641e6a0128ecc1255e1fbc0c20b2f7146
Vandopal <petya-pavlov-20001@mail.ru>
2025-09-26 20:17:39 +0500

    local save

--- src/store/todoSlice/todoSlice.js
+++ src/store/todoSlice/todoSlice.js
@@ -1,9 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const saveToLocalStorage = (state) => {
+  try {
+    localStorage.setItem('todos', JSON.stringify(state.todos));
+  } catch (err) {
+    console.error('Ошибка сохранения:', err);
+  }
+};
+
 const todoSlice = createSlice({
   name: 'todos',
-  initialState : {
-    todos : []
+  initialState: {
+    todos: JSON.parse(localStorage.getItem('todos')) || []
   },
   reducers: {
     addTodo: (state, action) => {
@@ -13,18 +21,21 @@ const todoSlice = createSlice({
         completed: false
       };
       state.todos.push(newTodo);
+      saveToLocalStorage(state);
     },
     toggleTodo: (state, action) => {
       const todo = state.todos.find(todo => todo.id === action.payload);
       if (todo) {
         todo.completed = !todo.completed;
+        saveToLocalStorage(state);
       }
     },
     deleteTodo: (state, action) => {
       state.todos = state.todos.filter(todo => todo.id !== action.payload);
+      saveToLocalStorage(state);
     }
   }
 });
 
 export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
-export default todoSlice.reducer
+export default todoSlice.reducer;
